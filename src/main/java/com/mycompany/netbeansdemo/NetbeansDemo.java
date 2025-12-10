a0d70144415844fa42792fdb7dedb01101458c4b
User <User@Q2C2002-PC01>
2025-12-10 09:44:52 +0800

    First commit

---
+++ src/main/java/com/mycompany/netbeansdemo/NetbeansDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.netbeansdemo;
+
+/**
+ *
+ * @author Lee Beng Yong
+ */
+public class NetbeansDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
